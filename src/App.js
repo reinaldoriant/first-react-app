0702323213972d3fa7fcba45c7535cbfb42d34fa
reinaldoriant <reinaldoriant@gmail.com>
2021-02-16 12:40:55 +0700

    coba tampilin

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const userLogged = "Aldo";
+  const userLoggedJSX = <strong>Ruang Aldo For Future!</strong>;
+
+  let a =3;
+  let b =4;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{userLogged}</p>
+      <p>{userLoggedJSX}</p>
+
+      {a+b/200}
     </div>
   );
 }
